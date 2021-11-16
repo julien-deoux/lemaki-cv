eba3e0ee9da34cdc3536ab5ef377de20187de31f
Julien Déoux <juliendeoux@gmail.com>
2021-11-16 17:10:31 +0100

    Setup automatic generation of contacts from JSON

---
+++ js/generateIdentity.js
@@ -0,0 +1,65 @@
+const generateIdentity = (function (){
+  function generateContact(contact) {
+    const tag = document.createElement('li');
+    tag.classList.add('contact');
+
+    tag.innerHTML = `
+      <span class="${ contact.classes } contact__icon"></span>
+    ` + (
+        contact.link ? `
+        <a class="contact__link" href="${ contact.link }">
+          ${ contact.text }
+        </a>
+      ` : contact.text 
+    );
+
+    return tag;
+  }
+
+  function contactList(contactObject) {
+    const co = contactObject;
+    let result = [];
+    if (co.phone) {
+      result.push({classes: "fas fa-phone", text: co.phone});
+    }
+    if (co.email) {
+      result.push({classes: "fas fa-envelope", text: co.email, link: "mailto:" + co.email});
+    }
+    if (co.website) {
+      result.push({classes: "fas fa-cloud", text: co.website, link: "https://" + co.website});
+    }
+    if (co.linkedin) {
+      result.push({classes: "fab fa-linkedin", text: "linkedin.com/in/" + co.linkedin, link: "https://linkedin.com/in/" + co.linkedin});
+    }
+    if (co.github) {
+      result.push({classes: "fab fa-github", text: "github.com/" + co.github, link: "https://github.com/" + co.github});
+    }
+    return result;
+  }
+
+  function generateContacts(identity) {
+    const tag = document.getElementById('contacts');
+    contactList(identity).forEach(contact => {
+      tag.append(generateContact(contact))
+    });
+  }
+
+  function generateName(identity) {
+    const tag = document.getElementById('name');
+    tag.innerText = identity.name;
+  }
+
+  return function() {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', handler);
+    xhr.open('GET', `content/identity.json`);
+    xhr.send();
+
+    function handler() {
+      const identity = JSON.parse(this.responseText);
+
+      generateContacts(identity);
+      generateName(identity)
+    }
+  }
+})();
